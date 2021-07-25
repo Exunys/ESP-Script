89e1fdadc7212555f0eb7c735287a53d6bc87ad3
Exunys <76539058+Exunys@users.noreply.github.com>
2021-07-26 01:56:59 +0200

    Script

--- ESP.lua
+++ ESP.lua
@@ -1,14 +1,3 @@
---[[
-
-		██╗░░░██╗██████╗░██╗░█████╗░░█████╗░░██████╗████████╗
-		██║░░░██║██╔══██╗██║██╔══██╗██╔══██╗██╔════╝╚══██╔══╝
-		██║░░░██║██████╦╝██║██║░░╚═╝███████║╚█████╗░░░░██║░░░
-		██║░░░██║██╔══██╗██║██║░░██╗██╔══██║░╚═══██╗░░░██║░░░
-		╚██████╔╝██████╦╝██║╚█████╔╝██║░░██║██████╔╝░░░██║░░░
-		░╚═════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░
-
-]]
-
 local function API_Check()
     if Drawing == nil then
         return "No"
@@ -21,7 +10,7 @@ local Find_Required = API_Check()
 
 if Find_Required == "No" then
     game:GetService("StarterGui"):SetCore("SendNotification",{
-        Title = "Ubicast Developer";
+        Title = "Exunys Developer";
         Text = "ESP script could not be loaded because your exploit is unsupported.";
         Duration = math.huge;
         Button1 = "OK"
@@ -54,7 +43,7 @@ _G.TextFont = Drawing.Fonts.UI   -- The font of the text. (UI, System, Plex, Mon
 _G.DisableKey = Enum.KeyCode.Q   -- The key that disables / enables the ESP.
 
 local function CreateESP()
-    for _, v in pairs(Players:GetPlayers()) do
+    for _, v in next, Players:GetPlayers() do
         if v.Name ~= Players.LocalPlayer.Name then
             local ESP = Drawing.new("Text")
 
@@ -172,7 +161,7 @@ UserInputService.InputBegan:Connect(function(Input)
         
         if _G.SendNotifications == true then
             game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "Ubicast Developer";
+                Title = "Exunys Developer";
                 Text = "The ESP's visibility is now set to "..tostring(_G.ESPVisible)..".";
                 Duration = 5;
             })
@@ -187,7 +176,7 @@ end)
 if Success and not Errored then
     if _G.SendNotifications == true then
         game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "Ubicast Developer";
+            Title = "Exunys Developer";
             Text = "ESP script has successfully loaded.";
             Duration = 5;
         })
@@ -195,12 +184,12 @@ if Success and not Errored then
 elseif Errored and not Success then
     if _G.SendNotifications == true then
         game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "Ubicast Developer";
+            Title = "Exunys Developer";
             Text = "ESP script has errored while loading, please check the developer console! (F9)";
             Duration = 5;
         })
     end
-    TestService:Message("The ESP script has errored, please notify Ubicast with the following information :")
+    TestService:Message("The ESP script has errored, please notify Exunys with the following information :")
     warn(Errored)
     print("!! IF THE ERROR IS A FALSE POSITIVE (says that a player cannot be found) THEN DO NOT BOTHER !!")
 end
